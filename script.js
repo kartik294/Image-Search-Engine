c019a9c138b0fc6e476cff95f435d78612935470
Kartik <kartikgeu@gmail.com>
2023-12-02 15:56:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const accessKey = "8PztxcY2d-OjKJEFhinQDRxQVg3_6cPR79OSPGYlV-M";
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`; // Add per_page=12
+  const response = await fetch(url);
+  const data = await response.json();
+  const results = data.results;
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
+
+  showMoreBtn.style.display = "block"; // Show the button after images are loaded
+}
+
+searchForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  page = 1;
+  searchResult.innerHTML = ""; // Clear previous search results
+  await searchImages();
+});
+
+showMoreBtn.addEventListener("click", () => {
+  page++;
+  searchImages();
+});
